0555b0da68dc70e4859c313dc892718768c85afc
programando <jhonjamesmg@hotmail.com>
2021-03-28 13:00:20 -0500

    Login - Sanctum

--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
-      Usuario autenticado
+
+      Usuario autenticado <br>
       {{ user }}
-      <form @submit.prevent="login"> <br>
+<br>
+          <button v-if="user.email" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"  
+        @click="logout" > Cerrar sesión</button>  
+
+      <form   v-else @submit.prevent="login"> <br>
           <input  v-model="form.email" type="email" placeholder="email"> <br>
           <input  v-model="form.password" type="password" placeholder="password"><br>
           <button > Login</button>
@@ -20,16 +25,33 @@ export default {
           password:'password',
       }
   }),
- 
+  created(){ 
+        this.usuarioAutenticado(); 
+  },
+
   methods: {
-        login() {
-            Users.login ( this.form )
-            .then( response =>{
-                console.log(  response);
+        logout() {
+            Users.logout()
+            .then ( () => {
+                this.user = {};
             })
-            
-        }
-  },  
+        },
+        usuarioAutenticado(){
+                Users.auth() 
+                .then ( response => {
+                   this.user = response.data; 
+                })
+        },
+        login() {
+                Users.getCokie()
+                .then ( ()=> {
+                     Users.login ( this.form )
+                      .then( response =>{
+                        this.user = response.data;
+                    }) 
+                })
+             }  
+    }
 
 
 }
